e63b6725901cfad61aa51d4bbd7423cfbba05d0e
Lucero Valdez <lvaldezd.isc91@gmail.com>
2023-10-15 17:41:40 -0600

    Agregar items 2

--- 10-Login/src/app/app-routing.module.ts
+++ 10-Login/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ROUTES } from '../../../04-spotiApp/src/app/app.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'**', redirectTo:'Login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 10-Login/src/app/app.module.ts
+++ 10-Login/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
